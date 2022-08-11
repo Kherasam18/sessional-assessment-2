36a8aecb8a4a4054ecd109bb618302fac77a1487
Kherasam18 <kherasam18@gmail.com>
2022-08-12 03:37:20 +0530

    Editing Ajax part

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var xmlhttp = new XMLHttpRequest();
+// api link
 const url = "https://jsonplaceholder.typicode.com/photos?utm_source=Mailerlite&utm_medium=E-mail&utm_campaign=Test%20Series&utm_term=2022-08-09";
 
 
